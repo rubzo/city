d7443477b100281339cc9a33dd0b5a3261c40550
Stephen Kyle <jimmykane@gmail.com>
2015-12-05 01:23:14 +0000

    Periodic rendering, derelict buildings, etc.

--- city.py
+++ city.py
@@ -2,6 +2,7 @@
 
 import time
 import random
+import sys
 
 class Agent:
     def __init__(self):
@@ -21,15 +22,41 @@ class Blocker(Agent):
 
 class R(Agent):
     def __init__(self):
-        self.icon = "h"
+        self.icon = '\033[92m' + "R" + '\033[0m'
+
+        self.chance_become_derelict = 0.001
+
+    def check_for_industrial(self, world):
+        return isinstance(world.world.get((self.x, self.y - 1)), I) or \
+            isinstance(world.world.get((self.x, self.y + 1)), I) or \
+            isinstance(world.world.get((self.x - 1, self.y)), I) or \
+            isinstance(world.world.get((self.x + 1, self.y)), I)
+
+    def update(self, world):
+        if self.check_for_industrial(world) and random.random() < self.chance_become_derelict:
+            derelict = Derelict()
+            world.spawnAgent(derelict, self.x, self.y)
+            return ("dead", 0, 0)
+        return ("alive", self.x, self.y)
+
+class Derelict(Agent):
+    def __init__(self):
+        self.icon = '\033[95m' + "x" + '\033[0m'
+
+        self.chance_cleanup = 0.0005
+
+    def update(self, world):
+        if random.random() < self.chance_cleanup:
+            return ("dead", 0, 0)
+        return ("alive", self.x, self.y)
 
 class C(Agent):
     def __init__(self):
-        self.icon = "c"
+        self.icon = '\033[94m' + "C" + '\033[0m'
 
 class I(Agent):
     def __init__(self):
-        self.icon = "i"
+        self.icon = '\033[93m' + "I" + '\033[0m'
 
 class Road(Agent):
     def __init__(self):
@@ -37,9 +64,6 @@ class Road(Agent):
 
         self.vertical = True
         self.junction = False
-
-        self.grown = False
-        self.filled_up = False
 
         self.chance_spawn_junction = 0.09
         self.chance_spawn_building = 0.001
@@ -69,27 +93,20 @@ class Road(Agent):
             world.spawnAgent(building, x, y)
 
     def update(self, world):
-        if not self.grown:
-            if self.junction:
-                self.check_and_grow(world, self.x, self.y - 1, True)
-                self.check_and_grow(world, self.x, self.y + 1, True)
-                self.check_and_grow(world, self.x - 1, self.y, False)
-                self.check_and_grow(world, self.x + 1, self.y, False)
+        if self.junction:
+            self.check_and_grow(world, self.x, self.y - 1, True)
+            self.check_and_grow(world, self.x, self.y + 1, True)
+            self.check_and_grow(world, self.x - 1, self.y, False)
+            self.check_and_grow(world, self.x + 1, self.y, False)
+        else:
+            if self.vertical:
+                self.check_and_grow(world, self.x, self.y - 1)
+                self.check_and_grow(world, self.x, self.y + 1)
             else:
-                if self.vertical:
-                    self.check_and_grow(world, self.x, self.y - 1)
-                    self.check_and_grow(world, self.x, self.y + 1)
-                else:
-                    self.check_and_grow(world, self.x - 1, self.y)
-                    self.check_and_grow(world, self.x + 1, self.y)
+                self.check_and_grow(world, self.x - 1, self.y)
+                self.check_and_grow(world, self.x + 1, self.y)
 
-        if not self.filled_up:
-            if self.vertical and world.world.get((self.x - 1, self.y)) != None and world.world.get((self.x + 1, self.y)) != None:
-                self.filled_up = True
-            if not self.vertical and world.world.get((self.x, self.y - 1)) != None and world.world.get((self.x, self.y + 1)) != None:
-                self.filled_up = True
-
-        if not self.junction and not self.filled_up:
+        if not self.junction:
             if random.random() < self.chance_spawn_building:
                 if self.vertical:
                     if random.random() < 0.5:
@@ -106,6 +123,7 @@ class Road(Agent):
 
 class World:
     def __init__(self):
+        # TODO: Control access to world
         self.world = dict()
         self.next_world = dict()
         self.x_min = 0
@@ -168,11 +186,25 @@ for i in xrange(-20, 21):
     blocker = Blocker()
     world.seedAgent(blocker, i, 20)
 
+realtime = len(sys.argv) > 1 and sys.argv[1] == "realtime"
+
+t = time.time()
+
 steps = 0
 while True:
-   steps += 1
-   world.update()
-   world.draw()
-   print("Steps: {}".format(steps))
-   time.sleep(1.0/30)
+    steps += 1
 
+    world.update()
+
+    if realtime:
+        world.draw()
+        print("Steps: {}".format(steps))
+        time.sleep(1.0/20)
+    else:
+        if steps % 1000 == 0:
+            world.draw()
+            print("Steps: {}".format(steps))
+            new_t = time.time()
+            elapsed_time = new_t - t
+            t = new_t
+            print("Steps per second: {}".format(1000 / elapsed_time))
