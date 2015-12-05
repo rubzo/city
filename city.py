e86baa17fc990a8d9002dde45ae89c9c61cba84d
Stephen Kyle <jimmykane@gmail.com>
2015-12-05 17:41:39 +0000

    Improvements to roads.

--- city.py
+++ city.py
@@ -4,7 +4,7 @@ import time
 import random
 import sys
 
-class Agent:
+class Agent(object):
     def __init__(self):
         self.x = 0
         self.y = 0
@@ -20,17 +20,17 @@ class Blocker(Agent):
     def __init__(self):
         self.icon = "#"
 
-class R(Agent):
+class Residential(Agent):
     def __init__(self):
         self.icon = '\033[92m' + "R" + '\033[0m'
 
         self.chance_become_derelict = 0.001
 
     def check_for_industrial(self, world):
-        return isinstance(world.world.get((self.x, self.y - 1)), I) or \
-            isinstance(world.world.get((self.x, self.y + 1)), I) or \
-            isinstance(world.world.get((self.x - 1, self.y)), I) or \
-            isinstance(world.world.get((self.x + 1, self.y)), I)
+        return isinstance(world.world.get((self.x, self.y - 1)), Industrial) or \
+            isinstance(world.world.get((self.x, self.y + 1)), Industrial) or \
+            isinstance(world.world.get((self.x - 1, self.y)), Industrial) or \
+            isinstance(world.world.get((self.x + 1, self.y)), Industrial)
 
     def update(self, world):
         if self.check_for_industrial(world) and random.random() < self.chance_become_derelict:
@@ -50,76 +50,98 @@ class Derelict(Agent):
             return ("dead", 0, 0)
         return ("alive", self.x, self.y)
 
-class C(Agent):
+class Commercial(Agent):
     def __init__(self):
         self.icon = '\033[94m' + "C" + '\033[0m'
 
-class I(Agent):
+class Industrial(Agent):
     def __init__(self):
         self.icon = '\033[93m' + "I" + '\033[0m'
 
 class Road(Agent):
     def __init__(self):
-        self.icon = "?"
+        pass
 
-        self.vertical = True
-        self.junction = False
-
+class VerticalRoad(Road):
+    def __init__(self):
+        self.icon = "|"
+        self.chance_extend = 0.05
         self.chance_spawn_junction = 0.09
         self.chance_spawn_building = 0.001
 
-    def getSelf(self):
-        if self.junction:
-            return "+"
-        elif self.vertical:
-            return "|"
-        return "-"
-
-    def check_and_grow(self, world, x, y, spawn_vertical = False):
-        if world.world.get((x, y)) == None:
-            new_road = Road()
-            if not self.junction and random.random() < self.chance_spawn_junction:
-                new_road.junction = True
-            elif not self.junction:
-                new_road.vertical = self.vertical
-            else:
-                new_road.vertical = spawn_vertical
-
-            world.spawnAgent(new_road, x, y)
-
-    def check_and_spawn_building(self, world, x, y):
-        if world.world.get((x, y)) == None:
-            building = random.choice([R(), C(), I()])
-            world.spawnAgent(building, x, y)
-
     def update(self, world):
-        if self.junction:
-            self.check_and_grow(world, self.x, self.y - 1, True)
-            self.check_and_grow(world, self.x, self.y + 1, True)
-            self.check_and_grow(world, self.x - 1, self.y, False)
-            self.check_and_grow(world, self.x + 1, self.y, False)
-        else:
-            if self.vertical:
-                self.check_and_grow(world, self.x, self.y - 1)
-                self.check_and_grow(world, self.x, self.y + 1)
+        if random.random() < self.chance_extend:
+            if random.random() < self.chance_spawn_junction:
+                world.checkEmptyAndSpawnThisType(CrossRoad, self.x, self.y - 1)
             else:
-                self.check_and_grow(world, self.x - 1, self.y)
-                self.check_and_grow(world, self.x + 1, self.y)
-
-        if not self.junction:
-            if random.random() < self.chance_spawn_building:
-                if self.vertical:
-                    if random.random() < 0.5:
-                        self.check_and_spawn_building(world, self.x - 1, self.y)
-                    else:
-                        self.check_and_spawn_building(world, self.x + 1, self.y)
-                else:
-                    if random.random() < 0.5:
-                        self.check_and_spawn_building(world, self.x, self.y - 1)
-                    else:
-                        self.check_and_spawn_building(world, self.x, self.y + 1)
+                world.checkEmptyAndSpawnSameType(self, self.x, self.y - 1)
+        if random.random() < self.chance_extend:
+            if random.random() < self.chance_spawn_junction:
+                world.checkEmptyAndSpawnThisType(CrossRoad, self.x, self.y + 1)
+            else:
+                world.checkEmptyAndSpawnSameType(self, self.x, self.y + 1)
+        if random.random() < self.chance_spawn_building:
+            building_choice = random.choice([Residential, Commercial, Industrial])
+            world.checkEmptyAndSpawnThisType(building_choice, self.x - 1, self.y)
+        if random.random() < self.chance_spawn_building:
+            building_choice = random.choice([Residential, Commercial, Industrial])
+            world.checkEmptyAndSpawnThisType(building_choice, self.x - 1, self.y)
 
         return ("alive", self.x, self.y)
+
+class HorizontalRoad(Road):
+    def __init__(self):
+        self.icon = "-"
+        self.chance_extend = 0.05
+        self.chance_spawn_junction = 0.09
+        self.chance_spawn_building = 0.001
+
+    def update(self, world):
+        if random.random() < self.chance_extend:
+            if random.random() < self.chance_spawn_junction:
+                world.checkEmptyAndSpawnThisType(CrossRoad, self.x - 1, self.y)
+            else:
+                world.checkEmptyAndSpawnSameType(self, self.x - 1, self.y)
+        if random.random() < self.chance_extend:
+            if random.random() < self.chance_spawn_junction:
+                world.checkEmptyAndSpawnThisType(CrossRoad, self.x + 1, self.y)
+            else:
+                world.checkEmptyAndSpawnSameType(self, self.x + 1, self.y)
+        if random.random() < self.chance_spawn_building:
+            building_choice = random.choice([Residential, Commercial, Industrial])
+            world.checkEmptyAndSpawnThisType(building_choice, self.x, self.y - 1)
+        if random.random() < self.chance_spawn_building:
+            building_choice = random.choice([Residential, Commercial, Industrial])
+            world.checkEmptyAndSpawnThisType(building_choice, self.x, self.y + 1)
+
+        return ("alive", self.x, self.y)
+
+class CrossRoad(Road):
+    def __init__(self):
+        self.icon = "+"
+        self.chance_extend = 0.05
+        self.chance_spawn_junction = 0.09
+        self.chance_spawn_building = 0.001
+
+    def update(self, world):
+        if random.random() < self.chance_extend:
+            world.checkEmptyAndSpawnThisType(HorizontalRoad, self.x - 1, self.y)
+        if random.random() < self.chance_extend:
+            world.checkEmptyAndSpawnThisType(HorizontalRoad, self.x + 1, self.y)
+        if random.random() < self.chance_extend:
+            world.checkEmptyAndSpawnThisType(VerticalRoad, self.x, self.y - 1)
+        if random.random() < self.chance_extend:
+            world.checkEmptyAndSpawnThisType(VerticalRoad, self.x, self.y + 1)
+
+
+        return ("alive", self.x, self.y)
+
+
+    #def check_and_spawn_building(self, world, x, y):
+    #    if world.world.get((x, y)) == None:
+    #        building = random.choice([Residential(), Commercial(), Industrial()])
+    #        world.spawnAgent(building, x, y)
+
 
 class World:
     def __init__(self):
@@ -151,6 +173,16 @@ class World:
                 self.next_world[(x, y)] = agent
         self.world = self.next_world
 
+    def checkEmptyAndSpawnSameType(self, agent, x, y):
+        if self.world.get((x, y)) == None:
+            new_agent = type(agent)()
+            self.spawnAgent(new_agent, x, y)
+
+    def checkEmptyAndSpawnThisType(self, agent_type, x, y):
+        if self.world.get((x, y)) == None:
+            new_agent = agent_type()
+            self.spawnAgent(new_agent, x, y)
+
     def seedAgent(self, agent, x, y):
         agent.x = x
         agent.y = y
@@ -174,7 +206,7 @@ class World:
         print("Agent count: {}".format(len(self.world)))
 
 world = World()
-road = Road()
+road = VerticalRoad()
 world.seedAgent(road, 0, 0)
 for i in xrange(-20, 21):
     blocker = Blocker()
